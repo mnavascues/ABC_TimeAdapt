eea836767a38795097d847e12722a54fe5a8df04
Miguel Navascues <miguel.navascues@inra.fr>
2020-09-24 11:13:57 +0200

    Changed format for input file with sample information to be CSV

--- src/funABC.R
+++ src/funABC.R
@@ -8,8 +8,8 @@
 # Uppsala universitet & INRAE
 # 2020
 
-read_sample_info <- function(file="data/SampleInfoTest.txt"){
-  info <- read.table(file,header=T,stringsAsFactors=F)
+read_sample_info <- function(file="data/SampleInfoTest.csv"){
+  info <- read.table(file,header=T,stringsAsFactors=F,sep = ",")
   return( list(id            = info$sample,
                age14C        = info$age14C,
                age14Cerror   = info$age14Cerror,
