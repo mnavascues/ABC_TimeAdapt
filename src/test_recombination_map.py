4d01599404b86f661ea351939063d6e0fdcd6caf
Miguel Navascues <miguel.navascues@inrae.fr>
2020-09-30 09:05:04 +0200

    slim & python scripts to test compatibility of recombination between slim & msprime

--- src/test_recombination_map.py
+++ src/test_recombination_map.py
@@ -1,5 +1,9 @@
+# run
+# python3 src/test_recombination_map.py 7
+
 import msprime
 import pyslim
+import sys
 
 # read recombination map and load it for msprime model
 file_recomb_map = open("data/recombination_map_msprime.txt", "r")
@@ -10,11 +14,12 @@ for line in file_recomb_map:
     positions.append(int(p))
     rates.append(float(r))
 
-num_of_chr = 7
+num_of_chr = int(sys.argv[1])
 
 recomb_map = msprime.RecombinationMap(positions=positions[0:(num_of_chr*2)],
                                       rates = rates[0:(num_of_chr*2)])
 
 # read tree, recapitate & add mutations
-treesq = pyslim.load("results/slim_test.tree")
+treesq = pyslim.load("results/test_recombination_map.tree")
 treesq = treesq.recapitate(Ne=200, recombination_map=recomb_map, model="dtwf")
+
