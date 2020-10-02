04252052e0df19b9cc0dcb5769b3bc0d97d21dd3
Miguel Navascues <miguel.navascues@inra.fr>
2020-10-02 11:12:37 +0200

    recapitate by genome intervals instead of whole genome, as suggested by Peter Ralph

--- src/add.mutations.py
+++ src/add.mutations.py
@@ -13,7 +13,7 @@ def main():
     sample_id, coverage, is_ancient, is_modern, is_dr, total_ancient, sample_size, t0 = read_sample_info()
 
     ttratio = 2.0/1.0
-    i = 1
+    sim_i = 1
     batch_id = 1
     project = "test"
     N = 200
@@ -26,82 +26,102 @@ def main():
 
     np.random.seed(seed)
 
-    positions, rates = read_recombination_map()
-    recomb_map = msprime.RecombinationMap(positions=positions, rates=rates)
-
     # add demography here
     demogr_event = [msprime.PopulationParametersChange(time=1000, initial_size=300, population_id=0)]
 
     # read tree, recapitate & add mutations
-    treesq = pyslim.load("results/" + project + "/" + str(batch_id) + "/slim" + str(i) + ".tree")
-    treesq = treesq.recapitate(Ne=N, recombination_map=recomb_map, demographic_events=demogr_event, model="dtwf")
+    treesq = pyslim.load("results/" + project + "/" + str(batch_id) + "/slim" + str(sim_i) + ".tree")
+    # tree = treesq.first()
+    # print(tree.draw(format="unicode"))
 
-    sample_ind = np.random.choice(treesq.individuals_alive_at(ts[0]), ss[0], replace=False)
+    # simplify tree sequence keeping nodes for the sampled individuals and their roots
+    sample_individuals = np.random.choice(treesq.individuals_alive_at(ts[0]), ss[0], replace=False)
     for x in range(1, na + 1):
-        sample_ind = np.concatenate([sample_ind, treesq.individuals_alive_at(ts[x])])
-    
+        sample_individuals = np.concatenate([sample_individuals, treesq.individuals_alive_at(ts[x])])
     keep_nodes = []
-    for i in sample_ind:
-        keep_nodes.extend(treesq.individual(i).nodes)
-    
-    treesq = treesq.simplify(keep_nodes)
-    
-    # check age of sample:  treesq.individual(0).time
-    
-    sample_ind = treesq.individuals_alive_at(ts[0])
-    for x in range(1, na + 1):
-        sample_ind = np.concatenate([sample_ind, treesq.individuals_alive_at(ts[x])])
+    for samp_i in sample_individuals:
+        keep_nodes.extend(treesq.individual(samp_i).nodes)
+    treesq = treesq.simplify(keep_nodes, keep_input_roots = True)
+    #sample_individuals = chr_arm_treesq.individuals_alive_at(ts[0])
+    #for x in range(1, na + 1):
+    #    sample_individuals = np.concatenate([sample_individuals, chr_arm_treesq.individuals_alive_at(ts[x])])
 
-    treesq = pyslim.SlimTreeSequence(msprime.mutate(treesq, rate=mu))
-    
-    ploidy = 2
-    n_samples = sample_ind.shape[0]
-    n_loci = treesq.num_mutations
-    
-    geno_data = allel.GenotypeArray(np.full((n_loci, n_samples, ploidy), -1), dtype='i1')
-    
-    # check age of sample:  treesq.individual(0).time
-    positions = []
-    locus = 0
-    for variant in treesq.variants():
-        positions.append(round(variant.position))
-        # print(positions)
-        var_genotypes = variant.genotypes
-        # print(var_genotypes)
-        num_reads = st.poisson.rvs(mu=coverage, size=n_samples)
-        transversion_SNP = True
-        if st.uniform.rvs() < ttratio/(ttratio+1):
-            transversion_SNP = False
-        # print(num_reads)
-        for i in range(0, 2 * n_samples, 2):
-            geno_data[locus, int(i / 2)] = snp_calling(true_genotype=var_genotypes[i:(i + 2)],
-                                                       f_num_reads=num_reads[int(i/2)],
-                                                       transversion=transversion_SNP)
-        locus = locus + 1
+    # read genome intervals (e.g. chromosome arms start and end)
+    # and recombination rates from file
+    start_chr_arm, end_chr_arm, rec_rate_chr_arm = read_genome_intervals()
+    num_of_genome_intervals = len(start_chr_arm)
 
-    ac = geno_data.count_alleles()
-    pi = allel.sequence_diversity(positions, ac, start=1, stop=1000000)
-    print(pi)
-    
-    pi_w, windows, n_bases, counts = allel.windowed_diversity(positions, ac, size=100000, start=1, stop=1000000)
-    n_obs, minmax, mean, var, skew, kurt = st.describe(pi_w)
-    print(n_obs)
-    print(minmax)
-    print(mean)
-    print(var)
-    print(skew)
-    print(kurt)
+    # loop over genome intervals: recapitate, mutate, calculate sumstats
+    for gi in range(0, num_of_genome_intervals):
+        genome_interval = np.array([[start_chr_arm[gi], end_chr_arm[gi]]])
+        gi_treesq = treesq.keep_intervals(genome_interval, simplify=False)
+        gi_treesq = gi_treesq.ltrim()
+        gi_treesq = pyslim.SlimTreeSequence(gi_treesq.rtrim())
+        gi_treesq = gi_treesq.recapitate(recombination_rate=rec_rate_chr_arm[0],
+                                         Ne=N,
+                                         demographic_events=demogr_event,
+                                         model="dtwf")
+        gi_treesq = pyslim.SlimTreeSequence(msprime.mutate(gi_treesq, rate=mu))
+
+        geno_data = empty_genotype_array(n_loci=gi_treesq.num_mutations,
+                                         n_samples=sample_size,
+                                         ploidy=2)
+        positions = []
+        locus = 0
+        for variant in gi_treesq.variants():
+            positions.append(round(variant.position))
+            #print(positions)
+            var_genotypes = variant.genotypes
+            #print("--------------------------------")
+            #print(var_genotypes)
+            num_reads = st.poisson.rvs(mu=coverage, size=sample_size)
+            transversion_SNP = True
+            if st.uniform.rvs() < ttratio / (ttratio + 1):
+                transversion_SNP = False
+            # print(num_reads)
+            for i in range(0, 2 * sample_size, 2):
+                geno_data[locus, int(i / 2)] = snp_calling(true_genotype=var_genotypes[i:(i + 2)],
+                                                           f_num_reads=num_reads[int(i / 2)],
+                                                           transversion=transversion_SNP)
+            locus = locus + 1
 
 
-# Read csv file with information on sample. Format of the file:
-# --------------------------------------------------------------------------
-# sampleID,           age14C,  age14Cerror,  year,  coverage,  damageRepair
-# B_Ju_hoan_North-4,  NA,      NA,           2010,  40.57,     TRUE
-# S_Ju_hoan_North-1,  NA,      NA,           2010,  46.49,     TRUE
-# BallitoBayA,        1980,    20,           NA,    12.94,     FALSE
-# BallitoBayB,        2110,    30,           NA,    1.25,      TRUE
-# --------------------------------------------------------------------------
+
+        ac = geno_data.count_alleles()
+        pi = allel.mean_pairwise_difference(ac)
+        n_obs, minmax, mean, var, skew, kurt = st.describe(pi)
+        print("Mean pw diff= " + str(mean) + " for genome interval " + str(gi))
+
+
+
+    #ac = geno_data.count_alleles()
+    #pi = allel.sequence_diversity(positions, ac, start=1, stop=1000000)
+    #print(pi)
+    
+    #pi_w, windows, n_bases, counts = allel.windowed_diversity(positions, ac, size=100000, start=1, stop=1000000)
+    #n_obs, minmax, mean, var, skew, kurt = st.describe(pi_w)
+    #print(n_obs)
+    #print(minmax)
+    #print(mean)
+    #print(var)
+    #print(skew)
+    #print(kurt)
+
+
 def read_sample_info(sample_info_file="data/SampleInfoTest.csv"):
+    '''
+    Read csv file with information on sample. Format of the file:
+    --------------------------------------------------------------------------
+    sampleID,           age14C,  age14Cerror,  year,  coverage,  damageRepair
+    B_Ju_hoan_North-4,  NA,      NA,           2010,  40.57,     TRUE
+    S_Ju_hoan_North-1,  NA,      NA,           2010,  46.49,     TRUE
+    BallitoBayA,        1980,    20,           NA,    12.94,     FALSE
+    BallitoBayB,        2110,    30,           NA,    1.25,      TRUE
+    --------------------------------------------------------------------------
+
+    :param sample_info_file: path of file
+    :return:
+    '''
     info = pd.read_csv(sample_info_file, skipinitialspace=True)
     sample_id = info.sampleID
     # age14C = info.age14C
@@ -116,18 +136,27 @@ def read_sample_info(sample_info_file="data/SampleInfoTest.csv"):
     t0 = max(info.year)
     return sample_id, coverage, is_ancient, is_modern, is_dr, total_ancient, sample_size, t0
 
+info_file = open("data/SampleInfoTest.txt", "r")
+for line in info_file:
+    line.split()
 
-# Read file with positions for start and end of chromosomes and
-# recombination rates for each chromosome. Format:
-# -------------------------------------
-# 0 1.14856e-08
-# 249218992 0.5
-# 249218993 1.10543e-08
-# 492309989 0.5
-# 492309990 1.12796e-08
-# 690184517 0.5
-# -------------------------------------
+
 def read_recombination_map(recombination_map_file="data/recombination_map_msprime.txt"):
+    '''
+    Read file with positions for start and end of chromosomes and
+    recombination rates for each chromosome. Format:
+    -------------------------------------
+    0 1.14856e-08
+    249218992 0.5
+    249218993 1.10543e-08
+    492309989 0.5
+    492309990 1.12796e-08
+    690184517 0.5
+    -------------------------------------
+
+    :param recombination_map_file:
+    :return:
+    '''
     file_recomb_map = open(recombination_map_file, "r")
     positions = []
     rates = []
@@ -138,12 +167,44 @@ def read_recombination_map(recombination_map_file="data/recombination_map_msprim
     return positions, rates
 
 
-# snp_calling function takes perfect simulated data from one locus of one diploid individual and
-# adds missing data and error according to the number of reads of the site, error rate of the
-# sequencing technology and, for ancient DNA not sequenced from damage repair (dr) libraries,
-# creates missing data for transition SNPs (since they cannot be distinguished from aDNA damage)
+def read_genome_intervals(genome_intervals_file="data/genome_intervals.txt"):
+    '''
+    read file with information on the starting position and end position of
+    each chromosome arm (centromeres removed)
+
+    :param genome_intervals_file:
+    :return:
+    '''
+    file_genome_intervals = open(genome_intervals_file, "r")
+    start = []
+    end = []
+    rates = []
+    for line in file_genome_intervals:
+        v1, v2, v3 = line.split()
+        start.append(int(v1))
+        end.append(int(v2))
+        rates.append(float(v3))
+    return start, end, rates
+
+
 def snp_calling(true_genotype, f_num_reads, error_rate=0.005, reads_th=1, score_th=10,
                 ratio_th=3, dr=True, transversion=True):
+    '''
+    snp_calling function takes perfect simulated data from one locus of one diploid individual and
+    adds missing data and error according to the number of reads of the site, error rate of the
+    sequencing technology and, for ancient DNA not sequenced from damage repair (dr) libraries,
+    creates missing data for transition SNPs (since they cannot be distinguished from aDNA damage)
+
+    :param true_genotype:
+    :param f_num_reads:
+    :param error_rate:
+    :param reads_th:
+    :param score_th:
+    :param ratio_th:
+    :param dr:
+    :param transversion:
+    :return:
+    '''
     if dr is False and transversion is False:
         genotype_call = [-1, -1]
     elif f_num_reads >= reads_th:
@@ -172,6 +233,19 @@ def snp_calling(true_genotype, f_num_reads, error_rate=0.005, reads_th=1, score_
     return genotype_call
 
 
+def empty_genotype_array(n_loci,n_samples,ploidy = 2):
+    '''
+    Creates a genotype array with all values as missing (-1) for a given number
+    of samples, loci and ploidy
+
+    :return: empty_ga
+    '''
+    empty_ga = allel.GenotypeArray(np.full((n_loci, n_samples, ploidy), -1), dtype='i1')
+    return empty_ga
+
+
 ###########################
 if __name__ == "__main__":
     main()
+
+
