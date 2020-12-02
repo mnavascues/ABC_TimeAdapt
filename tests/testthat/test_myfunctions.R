c90112467c7deb006599e7438885028470ddb6c2
Miguel Navascués <miguel.navascues@inrae.fr>
2020-12-02 15:36:38 +0100

    added list of function that are not yet tested

--- tests/testthat/test_myfunctions.R
+++ tests/testthat/test_myfunctions.R
@@ -1,11 +1,28 @@
 source("../../R/myfunctions.R")
 
+context("Command line arguments")
+
+# get_arguments()
+# print_arguments()
+
 context("Read info files")
 
-# read_sample_info("../../data/sample_info_test.txt")
-
+# check_file_header()
 test_that("Check header sends error when missing coloumn", {
   expect_error(check_file_header(c("a","b"),c("a")))
   expect_error(check_file_header(c("c"),c("a")))
   expect_error(check_file_header(c("c"),character()))
 })
+# read_sample_info("../../data/sample_info_test.txt")
+# read_genome_info()
+
+context("Ages of samples")
+
+# get_sample_cal_age_PDF()
+# check_ts_lower_gen_in_for_sim()
+
+context("Sample from priors")
+
+# sample_N_trajectory()
+# sample_demography_from_prior()
+
