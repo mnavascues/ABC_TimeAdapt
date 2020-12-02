697bfb1f2e1d637183b83080c6f90fe86a2dc123
Miguel Navascués <miguel.navascues@inrae.fr>
2020-12-02 15:03:53 +0100

    removed wrong test

--- tests/testthat/test_myfunctions.R
+++ tests/testthat/test_myfunctions.R
@@ -2,18 +2,7 @@ source("../../R/myfunctions.R")
 
 context("Read info files")
 
-#test_that("Verify type in sample info file", {
-#  info <- read_sample_info("../../data/sample_info_test.txt")
-#  expect_true(is.character(info$id))
-#  expect_true(is.numeric(info$age14C))
-#  expect_true(is.numeric(info$age14Cerror))
-#  expect_true(is.numeric(info$ageBCAD))
-#  expect_true(is.numeric(info$coverage))
-#  expect_true(is.logical(info$is_dr))
-#  expect_true(is.numeric(info$total_ancient))
-#  expect_true(is.numeric(info$size))
-#  expect_true(is.numeric(info$t0))
-#})
+# read_sample_info("../../data/sample_info_test.txt")
 
 test_that("Check header sends error when missing coloumn", {
   expect_error(check_file_header(c("a","b"),c("a")))
