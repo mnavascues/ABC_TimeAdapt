4193e268af4673e183131b0f0ef5958d78620f34
Miguel Navascués <miguel.navascues@inrae.fr>
2020-12-02 09:54:42 +0100

    correction of bug on path to file for tests

--- tests/testthat/test_myfunctions.R
+++ tests/testthat/test_myfunctions.R
@@ -10,7 +10,7 @@ test_that("Verify type in sample info file", {
   expect_true(is.numeric(read_sample_info("../../data/sample_info_test.txt")$coverage))
   # expect_true(is.logical(read_sample_info()$is_modern))
   # expect_true(is.logical(read_sample_info()$is_ancient))
-  expect_true(is.logical(read_sample_info()$is_dr))
+  expect_true(is.logical(read_sample_info("../../data/sample_info_test.txt")$is_dr))
   expect_true(is.numeric(read_sample_info("../../data/sample_info_test.txt")$total_ancient))
   expect_true(is.numeric(read_sample_info("../../data/sample_info_test.txt")$size))
   expect_true(is.numeric(read_sample_info("../../data/sample_info_test.txt")$t0))
