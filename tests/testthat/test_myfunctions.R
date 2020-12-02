b404da9f2ed10550e3dc8cd62890e9771ec671e1
Miguel Navascués <miguel.navascues@inrae.fr>
2020-12-02 14:40:53 +0100

    Wrong test, it is testing input file not the function

--- tests/testthat/test_myfunctions.R
+++ tests/testthat/test_myfunctions.R
@@ -2,18 +2,18 @@ source("../../R/myfunctions.R")
 
 context("Read info files")
 
-test_that("Verify type in sample info file", {
-  info <- read_sample_info("../../data/sample_info_test.txt")
-  expect_true(is.character(info$id))
-  expect_true(is.numeric(info$age14C))
-  expect_true(is.numeric(info$age14Cerror))
-  expect_true(is.numeric(info$ageBCAD))
-  expect_true(is.numeric(info$coverage))
-  expect_true(is.logical(info$is_dr))
-  expect_true(is.numeric(info$total_ancient))
-  expect_true(is.numeric(info$size))
-  expect_true(is.numeric(info$t0))
-})
+#test_that("Verify type in sample info file", {
+#  info <- read_sample_info("../../data/sample_info_test.txt")
+#  expect_true(is.character(info$id))
+#  expect_true(is.numeric(info$age14C))
+#  expect_true(is.numeric(info$age14Cerror))
+#  expect_true(is.numeric(info$ageBCAD))
+#  expect_true(is.numeric(info$coverage))
+#  expect_true(is.logical(info$is_dr))
+#  expect_true(is.numeric(info$total_ancient))
+#  expect_true(is.numeric(info$size))
+#  expect_true(is.numeric(info$t0))
+#})
 
 test_that("Verify header sends error when missing coloumn", {
   expect_error(verify_file_header(c("a","b"),c("a")))
