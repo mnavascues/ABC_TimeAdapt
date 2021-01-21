6039988909047dbd888ead2455415d5e44401d72
Miguel Navascués <miguel.navascues@inrae.fr>
2021-01-21 11:51:56 +0100

    remotes packages is installed only if it is not available

--- requirements.R
+++ requirements.R
@@ -1,5 +1,5 @@
-install.packages("remotes")
-library("remotes")
+if (!requireNamespace("remotes"))
+  install.packages("remotes")
 remotes::install_version("argparser", "0.6")
 #remotes::install_version("abcrf", "1.8.1")
 remotes::install_version("extraDistr", "1.8.11", INSTALL_opts = '--no-lock')
