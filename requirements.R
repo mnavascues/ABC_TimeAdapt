0122b34318e7c3b7430ac1680eedfc7673de2edb
Miguel Navascués <miguel.navascues@inrae.fr>
2021-03-02 13:29:31 +0100

    r packages versions updated to those available through conda

--- requirements.R
+++ requirements.R
@@ -1,7 +1,7 @@
 CRAN_repo = "https://cloud.r-project.org/" # change if needed
 if (!requireNamespace("remotes"))
   install.packages("remotes", repos=CRAN_repo)
-remotes::install_version("argparser", "0.6", repos=CRAN_repo)
+remotes::install_version("argparser", "0.4", repos=CRAN_repo)
 remotes::install_version("extraDistr", "1.8.11", INSTALL_opts = '--no-lock', repos=CRAN_repo)
-remotes::install_version("rcarbon", "1.3.1", repos=CRAN_repo)
+remotes::install_version("rcarbon", "1.2.0", repos=CRAN_repo)
 
