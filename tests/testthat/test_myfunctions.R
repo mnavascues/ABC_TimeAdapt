6dc97b1394531879689c9c3eff27f8bfdbc7a760
Miguel Navascués <miguel.navascues@inrae.fr>
2021-03-19 11:27:06 +0100

    added tests to check info files are read correctly

--- tests/testthat/test_myfunctions.R
+++ tests/testthat/test_myfunctions.R
@@ -1,7 +1,7 @@
 library(argparser, quietly=TRUE)
 source("../../R/myfunctions.R")
 
-context("Command line arguments")
+context("Command line arguments (simulations.R)")
 
 # get_arguments()
 test_that("Check for incomplete set of arguments", {
@@ -40,15 +40,49 @@ test_that("Check header sends error when missing coloumn", {
 })
 
 # read_sample_info("../sample_info_test.txt")
-test_that("Readings sample info file sends error when wrong data type", {
+test_that("Reading sample info file sends error when wrong data type", {
   fname <- tempfile()
   write("sampleID age14C age14Cerror year coverage damageRepair groups", fname)
   write("1 a a a a a a", fname, append=T)
   expect_error(read_sample_info(fname),paste("Wrong data type in file:",fname))
   unlink(fname)
 })
+test_that("Reading sample info file gets right values", {
+  fname <- tempfile()
+  write("sampleID age14C age14Cerror year coverage damageRepair groups", fname)
+  write("modern   NA     NA          2010 30.03    TRUE         0", fname, append=T)
+  write("ancient  1980   20          NA   10.01    TRUE         1", fname, append=T)
+  Sample <- read_sample_info(fname)
+  expect_equal(Sample$id,            c("modern", "ancient"))
+  expect_equal(Sample$age14C,        c(NA, 1980))
+  expect_equal(Sample$age14Cerror,   c(NA, 20))
+  expect_equal(Sample$ageBCAD,       c(2010, NA))
+  expect_equal(Sample$coverage,      c(30.03, 10.01))
+  expect_equal(Sample$is_modern,     c(T,F))
+  expect_equal(Sample$is_ancient,    c(F,T))
+  expect_equal(Sample$is_dr,         c(T,T))
+  expect_equal(Sample$total_ancient, 1)
+  expect_equal(Sample$size,          2)
+  expect_equal(Sample$t0,            2010)
+  unlink(fname)
+})
 
 # read_genome_info()
+test_that("Reading genome info file gets right values", {
+  fname <- tempfile()
+  write("chromosome_end  recombination_rate", fname)
+  write("500000	         1.11E-09", fname, append=T)
+  write("1000000         1.01E-09", fname, append=T)
+  Genome <- read_genome_info(fname)
+  expect_equal(Genome$number_of_chromosomes, 2)
+  expect_equal(Genome$L,                     1000000)
+  expect_equal(Genome$rec_map_SLiM[,1],      c(500000, 500001, 1000000, 1000001))
+  expect_equal(Genome$rec_map_SLiM[,2],      c(1.11E-09, 0.5, 1.01E-09, 0.5))
+  unlink(fname)
+})
+
+
+
 
 context("Ages of samples")
 
