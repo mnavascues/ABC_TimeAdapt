77f9bb5c16d415ac6534675a10b74007eb4aa3da
Miguel de Navascués <mnavascues@users.noreply.github.com>
2021-06-21 09:55:35 +0200

    using numpy not necessary

--- scripts/coalsim.py
+++ scripts/coalsim.py
@@ -1,7 +1,7 @@
 import sys
 import msprime
 import pyslim
-import numpy as np
+#import numpy as np
 import timeadapt
 
 ## how to run:
@@ -9,25 +9,29 @@ import timeadapt
 
 def main():
   # get options for project and simulation:
-  project, batch, sim, genome_file, sample_file, N, seed_coal, seed_mut = \
+  project, batch, sim, genome_file, sample_file, ss, chrono_order, N, mu, ttratio, seq_error, seed_coal, seed_mut = \
            timeadapt.get_options(proj_options_file = sys.argv[1], sim_options_file = sys.argv[2])
   
   # set random seed:
-  np.random.seed(seed_coal)
+  # np.random.seed(seed_coal) # (use seed directly on sim_ancestry, no need of numpy)
+
   # get recombination map:
   num_of_chr, chrom_rates, chrom_ends = timeadapt.get_genome_map(gf = genome_file)
+
   # make_recombination map:
   positions, rates = timeadapt.make_rec_map(nchr = num_of_chr,
                                             c_rates = chrom_rates,
                                             c_ends = chrom_ends)
   rate_map = msprime.RateMap(position = positions, rate = rates) 
+
   # simulate with msprime
   # https://tskit.dev/msprime/docs/latest/intro.html
   msp_ts = msprime.sim_ancestry(samples            = N[0],
                                 population_size    = N[0],
                                 model              = "dtwf",
                                 recombination_rate = rate_map,
-                                random_seed        = np.random.randint(1, 2 ^ 32 - 1))
+                                #random_seed        = np.random.randint(1, 2 ^ 32 - 1))
+                                random_seed        = seed_coal)
   # make tree sequence a SLiM tree sequence
   slim_ts = pyslim.annotate_defaults(msp_ts, model_type="WF", slim_generation=1)
   
